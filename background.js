3135973ba26be9621fbfb28946aa6e8e543e7351
Kaai Suzuki <suzydp@me.com>
2018-06-11 22:40:28 -0700

    Debug - send message from popup to background

--- background.js
+++ background.js
@@ -1,10 +1,3 @@
-const delay = 3 * 1000;
-const popupNotify = () => {
-    chrome.notifications.create(null, {
-        type: chrome.notifications.TemplateType.BASIC,
-        iconUrl: '/icon.png',
-        title: 'Yo!',
-        message:'お元気ですか',
-    });
-};
-setTimeout(() => popupNotify(), delay)
+chrome.runtime.onMessage.addListener((message)=>{
+  console.log('backgroundだよ',message);
+});
--- popup.js
+++ popup.js
@@ -1,13 +1,6 @@
 document.querySelector('.yo').onclick = (event) => {
-    const delay = 3 * 1000; //3 seconds
-    const popupNotify = () => {
-        event.target.textContent = 'Yay!';
-        chrome.notifications.create(null, {
-            type: chrome.notifications.TemplateType.BASIC,
-            iconUrl: '/icon.png',
-            title: 'Yo!',
-            message:'お元気ですか',
-        });
-    };
-    setTimeout(() => popupNotify(), delay)
+    const message = {
+        type : 'start'
+    }
+    chrome.runtime.sendMessage(message);
 };
