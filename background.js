b78a03433afc170a432f138034474457de5bc26d
Kaai Suzuki <suzydp@me.com>
2018-06-11 22:49:22 -0700

    Change code of background.js to activate extensions

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
 chrome.runtime.onMessage.addListener((message)=>{
-  console.log('backgroundだよ',message);
+    const delay = 1 * 1000 //1 seconds
+    setTimeout(() => {
+        chrome.notifications.create({
+            type: chrome.notifications.TemplateType.BASIC,
+            iconUrl: "icon.png",
+            title: "Yo!",
+            message: "HEELLLOOOO from background",
+        });
+    }, delay);
 });
